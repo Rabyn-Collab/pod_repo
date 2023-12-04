3c356ca7a7ca9d071cbb2b2ab8133ffc709b5654
Rabyn-Collab <rabyn900@gmail.com>
2023-12-04 15:22:50 +0545

    changes

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,13 +3,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:podsriver/constants/app_sizes.dart';
 import 'package:podsriver/models/note.dart';
 import 'package:podsriver/provider/riverpod_providers.dart';
+import 'package:podsriver/views/widgets/add_field.dart';
 
 
 
 class HomePage extends StatelessWidget {
    HomePage({super.key});
 
-   final textControl = TextEditingController();
+
+   final textControl1 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,26 +29,7 @@ class HomePage extends StatelessWidget {
               return Column(
                 children: [
                   AppSizes.gapH12,
-                  TextFormField(
-                    controller: textControl,
-
-                    onFieldSubmitted: (val) {
-
-                      final note = Note(
-                          title: val.trim(),
-                          createdAt: DateTime.now().toString()
-                      );
-                       ref.read(todoProvider.notifier).noteAdd(note);
-                       textControl.clear();
-                    },
-                    decoration: InputDecoration(
-                        hintText: 'Add some',
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 10),
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.today)
-                    ),
-                  ),
+                  AddField(ref: ref),
                   Expanded(
                       child: ListView.builder(
                           itemCount: notes.length,
@@ -59,7 +42,31 @@ class HomePage extends StatelessWidget {
                                   width: 100,
                                   child: Row(
                                     children: [
-                                      IconButton(onPressed: (){}, icon:Icon(Icons.edit)),
+                                      IconButton(
+                                          onPressed: (){
+                                            showDialog(context: context, builder: (context){
+                                              return AlertDialog(
+                                                title: Text('Edit'),
+                                                content: TextFormField(
+                                                  controller: textControl1..text = note.title,
+                                                ),
+                                                actions: [
+                                                  TextButton(onPressed: (){
+                                                    final newNote = Note(
+                                                        title: textControl1.text.trim(),
+                                                        createdAt: note.createdAt
+                                                    );
+                                                    Navigator.of(context).pop();
+                                                    ref.read(todoProvider.notifier).noteUpdate(newNote);
+                                                  }, child: Text('Update')),
+                                                  TextButton(onPressed: (){
+                                                    Navigator.of(context).pop();
+                                                  }, child: Text('Cancel'))
+                                                ],
+                                              );
+                                            });
+
+                                      }, icon:Icon(Icons.edit)),
                                       IconButton(
                                           onPressed: (){
                                             showDialog(context: context, builder: (context){
