b1ec3db6c5885c90737a47f07d8e6e127ba455e7
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2023-12-25 15:31:18 +0545

    ch

---
+++ lib/provider/auth_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:podsriver/api_service/auth_service.dart';
+
+
+
+
+class AuthProvider extends AsyncNotifier{
+
+  @override
+  FutureOr build() {
+
+  }
+
+  Future<void> userLogin({
+    required String email,
+    required String password
+  }) async{
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() => AuthService.userLogin(email: email, password: password));
+  }
+}
