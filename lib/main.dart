08818a8dec81318656bdaf524322506bce01133f
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-01-24 15:14:46 +0545

    ch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:podsriver/home_page.dart';
-
 
 
 
@@ -22,7 +20,7 @@ class Home extends StatelessWidget {
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: TodoListView(),
+
     );
   }
 }
